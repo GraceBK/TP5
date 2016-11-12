b9efef42710a8ed27439d12777a5de02168ac644
GraceBK <iris.b1991@gmail.com>
2016-11-12 10:49:40 +0100

    Recharger "à chaud" une nouvelle version d'un plugin
    Recharge d'une nouvelle version de classe Plugin déjà chargé
    ATTENTION avec le PATH du plugin sinon risque de ne pas trouver le bon plugin car ils sont tous chargé en meme temps

---
+++ src/exemple/Exemple4.java
@@ -0,0 +1,51 @@
+package exemple;
+
+/**
+ * Created by graceboukou on 12/11/2016.
+ */
+public class Exemple4 {
+    private static Plugin loadPlugin(String className) {
+        Class c = null;
+        try {
+            c = Class.forName(className);
+        }
+        catch(ClassNotFoundException e) {
+            System.err.println("Classe " + className + " non trouvee");
+            e.printStackTrace();
+            return null;
+        }
+
+        try {
+            Object o = c.newInstance();
+            return (Plugin) o;
+        }
+        catch(InstantiationException e) {
+            System.err.println("Erreur dans l'instantiation de la classe "
+                    + className);
+            e.printStackTrace();
+            return null;
+        }
+        catch(IllegalAccessException e) {
+            System.err.println("Erreur dans l'instantiation de la classe "
+                    + className);
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            /**
+             *
+             */
+            Plugin plugin = loadPlugin("Plugin1");
+            plugin.decrisToi();
+            try {
+                Thread.sleep(5000);
+            }
+            catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
